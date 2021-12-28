315b6096f66092a340bc00f7d40324f3fb8afb9d
Juan Yuco <juanfernandoyuco@gmail.com>
2021-12-27 20:02:34 -0500

    Incoprporación de las cors en el controlador de instructores

--- UniversityReact.API/Controllers/InstuctorController.cs
+++ UniversityReact.API/Controllers/InstuctorController.cs
@@ -5,11 +5,13 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace UniversityReact.API.Controllers
 {
     [Authorize]
     [RoutePrefix("api/Instructor")]
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class InstuctorController : ApiController
     {
         private BL.Instructor instructor = new BL.Instructor();
