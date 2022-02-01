10e43e38ee61e503b22eee85aac1a7d6f623f7d0
Juan Yuco <juanfernandoyuco@gmail.com>
2022-01-31 19:27:46 -0500

    CORS en Enrollment

--- UniversityReact.API/Controllers/EnrollmentController.cs
+++ UniversityReact.API/Controllers/EnrollmentController.cs
@@ -5,11 +5,13 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace UniversityReact.API.Controllers
 {
     [Authorize]
     [RoutePrefix("api/Enrollment")]
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class EnrollmentController : ApiController
     {
         private BL.Enrollment enrollment = new BL.Enrollment();
